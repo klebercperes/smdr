558809accb99a025fc62cc0d9c07eda1b9186bc0
klebercperes <kleber@peres.systems>
2025-02-19 14:41:59 +1000

    ttt

--- files/views.py
+++ files/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 
-# Create your views here.
+def upload_file(request):
+    if request.method == 'POST' and request.FILES['file']:
+        myfile = request.FILES['file']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'upload.html')
